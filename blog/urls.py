97e368057a295c4723ed87f4de665c3e07f87e24
Omotola Coker <omotola.coker.o@gmail.com>
2022-06-21 16:56:31 +0100

    update start blog app and document requirements

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+#We can cll this file anything since we created it, but by convention we call it urls
+#Here we will map our view function to our urls
+
+#from django.urs import path
+#from . import views
+
+#urlpatterns = path(route:str, view: *args: any, **kwargs: Any)
+#so that becomes path("blog/hello", views.say_hello)
