8caa1296da8bb7597f959a3e6e4fcba6fbe1c211
Omotola Coker <omotola.coker.o@gmail.com>
2022-06-22 08:10:51 +0100

    create post model and run migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
+
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200, blank=False, null=False)
+    Text = models.TextField()
+
+    Created_date: models.DateTimeField(auto_now_add=True)
+    Published_date = models.DateTimeField(auto_now_add=True)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+
+
 #from django.http import HttpResponse
 
-#def say_hello(request):
-    #return HttpResponse("Hello World")
-    
+# def say_hello(request):
+# return HttpResponse("Hello World")
+
 # Create your views here.
