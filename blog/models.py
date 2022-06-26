3aab1d460edde15e80d73f3ee32ca16143a05a3f
Omotola Coker <omotola.coker.o@gmail.com>
2022-06-26 11:39:21 +0100

    update author and create fields in Post models

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,6 @@ User = get_user_model()
 class Post(models.Model):
     Title = models.CharField(max_length=200, blank=False, null=False)
     Text = models.TextField()
-
-    Created_date: models.DateTimeField(auto_now_add=True)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
+    Created_date = models.DateTimeField(auto_now_add=True)
     Published_date = models.DateTimeField(auto_now_add=True)
